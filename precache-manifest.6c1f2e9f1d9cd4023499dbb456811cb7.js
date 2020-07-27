39a52ad7dc76e0b216aabc459a5241dbaae77d16
SaiKoushik1314 <sai.koushik1314@gmail.com>
2020-07-26 21:49:24 -0400

    Updates

---
+++ precache-manifest.6c1f2e9f1d9cd4023499dbb456811cb7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ba67f689295a6d2fd17",
+    "url": "/react-tutorial/static/css/main.900411f8.chunk.css"
+  },
+  {
+    "revision": "4ba67f689295a6d2fd17",
+    "url": "/react-tutorial/static/js/main.4ba67f68.chunk.js"
+  },
+  {
+    "revision": "99d7c59f45359249397b",
+    "url": "/react-tutorial/static/js/1.99d7c59f.chunk.js"
+  },
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/react-tutorial/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/react-tutorial/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/react-tutorial/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/react-tutorial/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/react-tutorial/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "d6d2f3b1c146b92623229bc442b37dbd",
+    "url": "/react-tutorial/index.html"
+  }
+];
